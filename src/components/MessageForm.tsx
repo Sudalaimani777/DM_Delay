50fff9d1465691fb1b16799ee3298ee60ed800b1
Sudalaimani777 <sualaimani5112007@gmail.com>
2025-08-02 12:45:48 +0530

    Almost completed but fixing the input issue

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -6,33 +6,67 @@ import { Textarea } from "./ui/textarea";
 
 const MessageForm = () => {
 
-    const [message, setMessage] = useState <string> ("");
-    const [delay, setDelay] = useState <number> (10);
-    const [isSending, setIsSending] = useState <boolean> (false);
-    const [timerId, setTimerId] = useState <NodeJS.Timeout | null> (null);
-    const [sentMessage, isSentMessage] = useState <string> ("");
+    const [message, setMessage] = useState<string>("");
+    const [delay, setDelay] = useState<number>(10);
+    const [isSending, setIsSending] = useState<boolean>(false);
+    const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null);
+    const [sentMessage, setSentMessage] = useState<string>("");
 
-  return (
-    <>
-        <section className="max-w-md mx-auto mt-[20px] p-6 border rounded-lg shadow-sm bg-amber-300 space-y-4">
-            <h2 className="text-2xl text-gray-800 font-bold">Dm Delay Button</h2>
+    const handleSend = () => {
+        setIsSending(true);
 
-            <Textarea 
-            placeholder="Enter Your Message"
-            value={"Hello Guys"}
-            />
-            
-            <Input 
-            type="number"
-            placeholder="Delay in seconds"
-            value={10}
-            />
+        const id = setTimeout(() => {
+            setSentMessage(message);
+            setMessage("");
+            setIsSending(false);
+        }, delay * 1000);
+        setTimerId(id)
+    }
 
-            <Button className="w-full text-white">Send with delay</Button>
+    const handleCancel = () => {
+        if (timerId) {
+            clearTimeout(timerId)
+        }
+        setIsSending(false);
+    }
 
-        </section>
-    </>
-  )
+    return (
+        <>
+            <section className="max-w-md mx-auto mt-[20px] p-6 border rounded-lg shadow-sm bg-amber-300 space-y-4">
+                <h2 className="text-2xl text-gray-800 font-bold">Dm Delay Button</h2>
+
+                <Textarea
+                    placeholder="Enter Your Message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+
+                <Input
+                    type="number"
+                    placeholder="Delay in seconds"
+                    value={delay}
+                    onChange={(e) => setDelay(Number(e.target.value))}
+                    disabled={isSending}
+                />
+
+                {!isSending ? (
+                    <Button className="w-full text-white" onClick={handleSend}>Send with delay</Button>
+                ) :
+                    (
+                        <Button className="w-full text-white" variant={"destructive"} onClick={handleCancel}>Cancel Sending</Button>
+                    )
+                }
+
+                {sentMessage && (
+                    <section className="bg-green-200 border rounded p-3 text-green-900">
+                        <p className="font-semibold">Message Sent Successfully :</p>
+                        <p>{sentMessage}</p>
+                    </section>
+                )}
+
+            </section>
+        </>
+    )
 }
 
 export default MessageForm;
