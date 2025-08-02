885d0b614ffd7bef37efa84614f531915a49e5e4
Sudalaimani777 <sualaimani5112007@gmail.com>
2025-08-02 12:08:18 +0530

    Completed the UI

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -1,10 +1,28 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 
 const MessageForm = () => {
+    
   return (
     <>
         <section className="max-w-md mx-auto mt-[20px] p-6 border rounded-lg shadow-sm bg-amber-300 space-y-4">
             <h2 className="text-2xl text-gray-800 font-bold">Dm Delay Button</h2>
+
+            <Textarea 
+            placeholder="Enter Your Message"
+            value={"Hello Guys"}
+            />
+            
+            <Input 
+            type="number"
+            placeholder="Delay in seconds"
+            value={10}
+            />
+
+            <Button className="w-full text-white">Send with delay</Button>
+
         </section>
     </>
   )
